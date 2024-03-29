d371dde349470ec537d81391b6bd74c4fdf649eb
kadetov18 <kadetovolegosnovnai@gmail.com>
2024-03-29 18:40:20 +0200

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const emailInput = form.querySelector('input[name="email"]');
   const messageInput = form.querySelector('textarea[name="message"]');
 
-  // Відслідковуємо введення користувача у поля форми
   form.addEventListener('input', () => {
     const formData = {
       email: emailInput.value.trim(),
@@ -12,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   });
 
-  // Перевіряємо стан локального сховища при завантаженні сторінки
   const savedData = localStorage.getItem('feedback-form-state');
   if (savedData) {
     const { email, message } = JSON.parse(savedData);
@@ -20,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     messageInput.value = message;
   }
 
-  // Під час сабміту форми
   form.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -29,11 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
       message: messageInput.value.trim(),
     };
 
-    // Перевіряємо, чи всі поля заповнені
     if (formData.email && formData.message) {
-      // Виводимо дані в консоль
       console.log(formData);
-      // Очищаємо сховище і поля форми
+
       localStorage.removeItem('feedback-form-state');
       form.reset();
     } else {
